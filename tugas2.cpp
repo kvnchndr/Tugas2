1ed06adf8bc718f14a7790c33043dcd781b39c25
kvnchndr <59856557+kvnchndr@users.noreply.github.com>
2020-01-18 21:26:00 +0700

    2nd changes

--- tugas2.cpp
+++ tugas2.cpp
@@ -6,26 +6,48 @@ int main(){
 	cout<<"Masukkan operasi yang diinginkan : \n1.Penambahan Bilangan\n2.Pengurangan Bilangan\n3.Perkalian Bilangan\n4.Turunan Polinomial\n";
 	int P,a,b,c;
 	cin>>P;
-	cout<<"\nMasukkan bilangan pertama\n";
-	cin>>a;
-	cout<<"Masukkan bilangan kedua\n";
-	cin>>b;
+
+	if (P<4){
+		cout<<"\nMasukkan bilangan pertama\n";
+		cin>>a;
+		cout<<"Masukkan bilangan kedua\n";
+		cin>>b;
+		if (P==1){
+			c=a+b;
+			cout<<"\nHasil penjumlahan: "<<c;
+		}
 	
-	if (P==1){
-		c=a+b;
-		cout<<"\nHasil penjumlahan: "<<c;
+		else if(P==2){
+			c=a-b;
+			cout<<"\nHasil pengurangan: "<<c;
+		}
+	
+		else if (P==3){
+			c=a*b;
+			cout<<"\nHasil perkalian: "<<c;
+		}
+	}
+	
+	else if (P==4){
+		int N;
+		cout<<"\nMasukkan pangkat tertinggi persamaan polinomial ";
+		cin>>N;
+		int koef1[N+1];
+		int koef2[N];
+		string tur="";
 		
-	}
-	
-	else if(P==2){
-		c=a-b;
-		cout<<"\nHasil pengurangan: "<<c;
+		for (int i=0;i<N+1;i++){
+			cout<<"\nKoefisien pangkat- "<<i<<" =";
+			cin>>koef1[i];
+		}
 		
+		for (int i=0;i<N;i++){
+			koef2[i]=koef1[i+1]*(i+1);
+		}
+		cout<<"\nHasil turunan persamaan polinomialnya adalah :";
+		for (int i=0;i<N;i++){
+			cout<<"\nKoefisien pangkat- "<<i<<" = ";
+			cout<<koef2[i];
+		}
 	}
-	
-	else if (P==3){
-		c=a*b;
-		cout<<"\nHasil perkalian: "<<c;
-	}
-	
 }
